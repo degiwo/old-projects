29720a6875bb1e97fbb8ee658f9f839ffbb7c115
Detlef Wong <d.wong93@hotmail.de>
2020-04-08 16:38:42 +0200

    bullets shoot down aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _update_screen(self):
         """Update images on the screen, and flip to new screen."""
         self.screen.fill(self.settings.bg_color)
