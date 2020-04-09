21ec5d37dbee7af3f1e4d5df2732561915092c99
Detlef Wong <d.wong93@hotmail.de>
2020-04-09 21:18:59 +0200

    enable Play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -57,6 +57,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
 
     def _check_keydown_events(self, event):
         """Respond to keypress."""
@@ -200,6 +203,11 @@ class AlienInvasion:
                 # Treat this the same as if the ship got hit.
                 self._ship_hit()
                 break
+
+    def _check_play_button(self, mouse_pos):
+        """"Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
 
 if __name__ == '__main__':
