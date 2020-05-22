1d9ec9d174ea439bbdf85f8c2506f15b05ac3512
Detlef Wong <d.wong93@hotmail.de>
2020-05-22 15:06:28 +0200

    added stringsAsFactors = FALSE for test in oldrel

--- tests/testthat/test-fct_projectview_server.R
+++ tests/testthat/test-fct_projectview_server.R
@@ -3,12 +3,14 @@ context("test server helper functions for projectview")
 test_that("entries without workpackage get a dummy workpackage", {
   test_data <- data.frame(
     workpackage = c(NA, "abc", "def", NA),
-    duration = 1:4
+    duration = 1:4,
+    stringsAsFactors = FALSE
   )
   result <- set_name_for_workpackages(test_data)
   expected_result <- data.frame(
     workpackage = c("OHNE AP", "abc", "def", "OHNE AP"),
-    duration = 1:4
+    duration = 1:4,
+    stringsAsFactors = FALSE
   )
 
   expect_equal(result, expected_result)
