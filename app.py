058d6e66f58cd01b5ccf9541b06678b1455c5e5b
Detlef Wong <d.wong93@hotmail.de>
2020-06-11 11:42:09 +0200

    label with current datetime

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtCore import QTimer, QDateTime
 
 
 class Window(QWidget):
@@ -7,7 +8,15 @@ class Window(QWidget):
         super().__init__()
         self.layout = QVBoxLayout()
 
-        self.layout.addWidget(QLabel('Test'))
+        self.lbl = QLabel('Test')
+        self.layout.addWidget(self.lbl)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+        self.update_time()
+
+    def update_time(self):
+        self.lbl.setText(QDateTime.currentDateTime().toString())
 
     def draw(self):
         self.setLayout(self.layout)
