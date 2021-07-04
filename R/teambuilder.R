d6b2ceb8c0d9e9bcae6b88953e09a83624d0614c
Detlef Wong <d.wong93@hotmail.de>
2021-07-04 11:05:13 +0200

    3 pokemon in team possible

--- R/teambuilder.R
+++ R/teambuilder.R
@@ -3,8 +3,23 @@ teambuilderUI <- function(id) {
 
     tagList(
         h2("Teambuilder"),
-        selectizeInput(ns("sel_pkmn"), "Choose Pok\u00E9mon", choices = NULL),
-        tableOutput(ns("tbl_pkmn")),
+        fluidRow(
+            column(
+                width = 4,
+                selectizeInput(ns("sel_pkmn1"), "Choose Pok\u00E9mon", choices = NULL),
+                tableOutput(ns("tbl_pkmn1"))
+            ),
+            column(
+                width = 4,
+                selectizeInput(ns("sel_pkmn2"), "Choose Pok\u00E9mon", choices = NULL),
+                tableOutput(ns("tbl_pkmn2"))
+            ),
+            column(
+                width = 4,
+                selectizeInput(ns("sel_pkmn3"), "Choose Pok\u00E9mon", choices = NULL),
+                tableOutput(ns("tbl_pkmn3"))
+            )
+        ),
         tableOutput(ns("tbl_types_defense"))
     )
 }
@@ -22,37 +37,44 @@ teambuilderServer <- function(id) {
         
         pkmn_team <- reactiveValues()
         
-        observe({
-            updateSelectizeInput(inputId = "sel_pkmn", choices = pokedex()$name)
+        lapply(1:3, function(i) {
+            observe({
+                updateSelectizeInput(inputId = paste0("sel_pkmn", i), choices = pokedex()$name)
+            })
         })
         
-        observeEvent(input$sel_pkmn, {
-            pokedex <- pokedex()
-            df <- pokedex[pokedex$name == input$sel_pkmn, c("name", "type", "type1", "type2")]
-            
-            pkmn_team$pkmn1[["name"]] <- input$sel_pkmn
-            pkmn_team$pkmn1[["type"]] <- df$type
-            pkmn_team$pkmn1[["defense"]] <- get_defense_multiplicators(df$type1[1], df$type2[1])
+        lapply(1:3, function(i) {
+            observeEvent(input[[paste0("sel_pkmn", i)]], {
+                pkmn <- input[[paste0("sel_pkmn", i)]]
+                pokedex <- pokedex()
+                df <- pokedex[pokedex$name == pkmn, c("name", "type", "type1", "type2")]
+                
+                pkmn_team[[paste0("pkmn", i)]][["name"]] <- pkmn
+                pkmn_team[[paste0("pkmn", i)]][["type"]] <- df$type
+                pkmn_team[[paste0("pkmn", i)]][["defense"]] <- get_defense_multiplicators(df$type1[1], df$type2[1])
+            })
         })
 
-        
+
         # Render output elements --------------------------------------------------
 
-        output$tbl_pkmn <- renderTable({
-            req(input$sel_pkmn)
-            df_def <- pkmn_team$pkmn1[["defense"]]
-            df <- data.frame(
-                name = pkmn_team$pkmn1[["name"]],
-                type = paste(pkmn_team$pkmn1[["type"]], collapse = ","),
-                weak = paste(df_def$type[df_def$multiplicator > 1], collapse = ","),
-                resists = paste(df_def$type[df_def$multiplicator < 1 & df_def$multiplicator > 0], collapse = ","),
-                immune = paste(df_def$type[df_def$multiplicator == 0], collapse = ",")
-            )
-            t(df)
-        }, rownames = TRUE, colnames = FALSE)
+        lapply(1:3, function(i) {
+            output[[paste0("tbl_pkmn", i)]] <- renderTable({
+                req(input[[paste0("sel_pkmn", i)]])
+                df_def <- pkmn_team[[paste0("pkmn", i)]][["defense"]]
+                df <- data.frame(
+                    name = pkmn_team[[paste0("pkmn", i)]][["name"]],
+                    type = paste(pkmn_team[[paste0("pkmn", i)]][["type"]], collapse = ","),
+                    weak = paste(df_def$type[df_def$multiplicator > 1], collapse = ","),
+                    resists = paste(df_def$type[df_def$multiplicator < 1 & df_def$multiplicator > 0], collapse = ","),
+                    immune = paste(df_def$type[df_def$multiplicator == 0], collapse = ",")
+                )
+                t(df)
+            }, rownames = TRUE, colnames = FALSE)
+        })
 
         output$tbl_types_defense <- renderTable({
-            req(input$sel_pkmn)
+            req(input$sel_pkmn1)
             df <- pkmn_team$pkmn1[["defense"]]
             df$weak <- ifelse(df$multiplicator > 1, 1, 0)
             df$resist <- ifelse(df$multiplicator < 1 & df$multiplicator > 0, 1, 0)
