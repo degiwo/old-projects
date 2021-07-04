179a50baa19302fd4aeaad57d95eb4ebf0e4885b
Detlef Wong <d.wong93@hotmail.de>
2021-07-04 15:40:07 +0200

    expand to 6 pkmn

--- R/teambuilder.R
+++ R/teambuilder.R
@@ -12,6 +12,15 @@ teambuilderUI <- function(id) {
                 )
             })
         ),
+        fluidRow(
+            lapply(4:6, function(i) {
+                column(
+                    width = 4,
+                    selectizeInput(ns(paste0("sel_pkmn", i)), "Choose Pok\u00E9mon", choices = NULL),
+                    tableOutput(ns(paste0("tbl_pkmn", i)))
+                )
+            })
+        ),
         tableOutput(ns("tbl_types_defense"))
     )
 }
@@ -26,7 +35,7 @@ teambuilderServer <- function(id) {
         
         pkmn_team <- reactiveValues()
         
-        lapply(1:3, function(i) {
+        lapply(1:6, function(i) {
             observe({
                 updateSelectizeInput(inputId = paste0("sel_pkmn", i), choices = pokedex()$name, options = list(
                     onInitialize = I('function() { this.setValue(""); }')
@@ -34,7 +43,7 @@ teambuilderServer <- function(id) {
             })
         })
         
-        lapply(1:3, function(i) {
+        lapply(1:6, function(i) {
             observeEvent(input[[paste0("sel_pkmn", i)]], {
                 pkmn <- input[[paste0("sel_pkmn", i)]]
                 pokedex <- pokedex()
@@ -43,13 +52,17 @@ teambuilderServer <- function(id) {
                 pkmn_team[[paste0("pkmn", i)]][["name"]] <- pkmn
                 pkmn_team[[paste0("pkmn", i)]][["type"]] <- df$type
                 pkmn_team[[paste0("pkmn", i)]][["defense"]] <- get_defense_multiplicators(df$type1[1], df$type2[1])
+                
+                # rename columns to prevent merge warnings
+                df <- pkmn_team[[paste0("pkmn", i)]][["defense"]]
+                names(pkmn_team[[paste0("pkmn", i)]][["defense"]])[names(df) != "type"] <- paste(names(df)[names(df) != "type"], i)
             })
         })
 
 
         # Render output elements --------------------------------------------------
 
-        lapply(1:3, function(i) {
+        lapply(1:6, function(i) {
             output[[paste0("tbl_pkmn", i)]] <- renderTable({
                 req(input[[paste0("sel_pkmn", i)]])
                 df_def <- pkmn_team[[paste0("pkmn", i)]][["defense"]]
