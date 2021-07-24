383d29ee065539ed43a01fa66302f052e4b2171e
Detlef Wong <d.wong93@hotmail.de>
2021-07-24 12:58:39 +0200

    choose moves for every pkmn in team

--- R/moveset.R
+++ R/moveset.R
@@ -3,21 +3,53 @@ movesetUI <- function(id) {
     
     tagList(
         h2("Moveset"),
-        textOutput(ns("txt_pkmn1")),
-        selectizeInput(ns("sel_pkmn1_move1"), label = "", choices = NULL)
+        fluidRow(
+            lapply(1:3, function(i) {
+                column(
+                    width = 4,
+                    textOutput(ns(paste0("txt_pkmn", i))),
+                    lapply(1:4, function(j) {
+                        selectizeInput(ns(paste0("sel_pkmn", i, "_move", j)), label = "", choices = NULL)
+                    })
+                )
+                
+            })
+        ),
+        fluidRow(
+            lapply(4:6, function(i) {
+                column(
+                    width = 4,
+                    textOutput(ns(paste0("txt_pkmn", i))),
+                    lapply(1:4, function(j) {
+                        selectizeInput(ns(paste0("sel_pkmn", i, "_move", j)), label = "", choices = NULL)
+                    })
+                )
+                
+            })
+        )
     )
 }
 
 movesetServer <- function(id, pkmn_team) {
     moduleServer(id, function(input, output, session) {
-        output$txt_pkmn1 <- renderText({
-            pkmn_team[[paste0("pkmn", 1)]][["name"]]
-        })
         
-        observe({
-            updateSelectizeInput(inputId = "sel_pkmn1_move1",
-                                 session = session,
-                                 choices = get_moves_list(pkmn_team[[paste0("pkmn", 1)]][["name"]]))
+        lapply(1:6, function(i) {
+            output[[paste0("txt_pkmn", i)]] <- renderText({
+                pkmn_team[[paste0("pkmn", i)]][["name"]]
+            })
+            
+            lapply(1:4, function(j) {
+                observe({
+                    updateSelectizeInput(inputId = paste0("sel_pkmn", i, "_move", j),
+                                         session = session,
+                                         choices = get_moves_list(pkmn_team[[paste0("pkmn", i)]][["name"]]),
+                                         selected = NULL,
+                                         options = list(onInitialize = I('function() { this.setValue(""); }'))
+                                         )
+                })
+            })
+
         })
+
     })
 }
