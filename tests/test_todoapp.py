2d163b4e4ccd812d5ee7aabf41bcf3643bacfa5c
degiwo <d.wong93@hotmail.de>
2022-05-30 21:54:04 +0200

    Test Typer application
    add test for version output

--- tests/test_todoapp.py
+++ tests/test_todoapp.py
@@ -1,5 +1,15 @@
-from todoapp import __version__
+"""Test Typer application"""
+from todoapp import __appname__, __version__, cli
+from typer.testing import CliRunner
+
+runner = CliRunner()
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_version_via_cli():
+    result = runner.invoke(cli.app, ["--version"])
+    assert result.exit_code == 0
+    assert f"{__appname__} v{__version__}\n" in result.stdout
