bda5ce7782faff1fa7dc092eff1b4a61058e6d81
degiwo <d.wong93@hotmail.de>
2022-07-30 16:59:14 +0200

    feat(app): add dropdown to choose Pokémon
    The dropdown lists all available Pokémon which can be get via Pokeapi

--- teambuilder/app.py
+++ teambuilder/app.py
@@ -14,11 +14,23 @@ def get_version_groups():
     return version_groups
 
 
+def get_all_pokemon():
+    all_pokemon = [
+        x.get("name")
+        for x in requests.get("https://pokeapi.co/api/v2/pokemon?limit=100000")
+        .json()
+        .get("results")
+    ]
+    return all_pokemon
+
+
 app = Dash(__name__)
 app.layout = html.Div(
     [
         html.H1("Teambuilder"),
         html.Label("Choose your version group:"),
         dcc.RadioItems(get_version_groups(), id="home-in-version-group"),
+        html.Br(),
+        dcc.Dropdown(get_all_pokemon(), id="home-in-pokemon"),
     ]
 )
