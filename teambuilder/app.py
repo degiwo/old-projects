852f1e8a2bf54f2837a4b4e31c884e7266fd1b03
degiwo <d.wong93@hotmail.de>
2022-07-30 16:33:41 +0200

    feat(app): add radiobuttons to choose version groups
    Simple Radiobuttons which options are get via request to Pokeapi without real functionality/callback

--- teambuilder/app.py
+++ teambuilder/app.py
@@ -1,6 +1,24 @@
 """First level layout of application"""
 
-from dash import Dash, html
+import requests
+from dash import Dash, dcc, html
+
+
+def get_version_groups():
+    version_groups = [
+        x.get("name")
+        for x in requests.get("https://pokeapi.co/api/v2/version-group")
+        .json()
+        .get("results")
+    ]
+    return version_groups
+
 
 app = Dash(__name__)
-app.layout = html.Div([html.H1("Teambuilder")])
+app.layout = html.Div(
+    [
+        html.H1("Teambuilder"),
+        html.Label("Choose your version group:"),
+        dcc.RadioItems(get_version_groups(), id="home-in-version-group"),
+    ]
+)
