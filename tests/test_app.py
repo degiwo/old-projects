1e30ac302be10b87e118501c29f3cde45a55eee7
degiwo <d.wong93@hotmail.de>
2022-08-15 18:17:43 +0200

    TEST: test app as html
    - add a test to check if app return as html site

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,3 +4,8 @@ from teambuilder.app import app
 
 def test_app_layout_is_html_div():
     assert isinstance(app.layout, html.Div)
+
+
+def test_app_layout_string_includes_html():
+    layout_string = app.index_string
+    assert "<html>" in layout_string
