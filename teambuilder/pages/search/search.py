f18ba3b9cbe77f54415715d9d19b104a39ad0ab3
degiwo <d.wong93@hotmail.de>
2022-08-15 18:37:36 +0200

    FEAT: add Offcanvas and toggle button
    - a Offcanvas can be toggled with a button
    - from the left side, a window pop ups
    - the window is intended as a place for the filters

--- teambuilder/pages/search/search.py
+++ teambuilder/pages/search/search.py
@@ -1,5 +1,6 @@
 import dash
-from dash import html
+import dash_bootstrap_components as dbc
+from dash import Input, Output, State, html
 
 dash.register_page(
     __name__,
@@ -12,5 +13,47 @@ def layout():
     return html.Div(
         [
             html.H1("Search"),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        html.P("In this section you can filter Pokémon."),
+                        width=3,
+                    ),
+                    dbc.Col(
+                        dbc.Button(
+                            "Open filters",
+                            id="search-button-open-filters",
+                            n_clicks=0,
+                        ),
+                        width=3,
+                    ),
+                ]
+            ),
+            dbc.Offcanvas(
+                html.P("TEST"),
+                id="search-offcanvas-filters",
+                title="Choose filters",
+                is_open=False,
+            ),
         ]
     )
+
+
+@dash.callback(
+    Output(
+        component_id="search-offcanvas-filters",
+        component_property="is_open",
+    ),
+    Input(
+        component_id="search-button-open-filters",
+        component_property="n_clicks",
+    ),
+    State(
+        component_id="search-offcanvas-filters",
+        component_property="is_open",
+    ),
+)
+def toggle_offcanvas_filters(click_on_button: int, canvas_state: bool) -> bool:
+    if click_on_button:
+        return not canvas_state
+    return canvas_state
