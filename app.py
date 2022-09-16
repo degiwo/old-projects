1408d0c505e0654a4d95feac3dd89b89b04b9721
degiwo <d.wong93@hotmail.de>
2022-09-16 18:10:25 +0200

    FEAT: send data to templates
    - optional arguments for render_template
    - these arguments can be used in html file

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template("home.html")
+    data = [
+        {"id": 1, "time": "08:00 - 09:00"},
+        {"id": 2, "time": "09:00 - 10:00"},
+        {"id": 3, "time": "10:00 - 10:45"}
+    ]
+    return render_template("home.html", data=data)
 
 
 @app.route("/about")
